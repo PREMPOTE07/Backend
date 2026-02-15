e0832804a1e3f073d57a546d9b27586fb205ec12
PREMPOTE07 <prempote07@gmail.com>
2026-02-15 13:53:41 +0530

    add utils files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path: './env'})
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 ,  () => {
+        console.log(`server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+     console.log("Mongodb connection failed !! ",error)
+})
 
 /*
 import express from 'express';
